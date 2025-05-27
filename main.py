632b4268b6b1d524f3f15e0a4091101295c61c9a
marlin69243 <marlin_69243@yahoo.com>
2025-05-27 17:47:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
 
 def send_telegram(message):
     if TELEGRAM_ENABLED:
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-        data = {"chat_id": CHAT_ID, "text": message}
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+        data = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
         try:
             response = requests.post(url, data=data, timeout=10)
             response.raise_for_status()
